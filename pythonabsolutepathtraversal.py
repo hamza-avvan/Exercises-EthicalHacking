e0f443a38e1163b6128e9627f39e401d115b7a62
Hamza Awan <101969363+hamza-avvan@users.noreply.github.com>
2023-11-28 10:07:06 +0500

    Add files via upload

---
+++ pythonabsolutepathtraversal.py
@@ -0,0 +1,17 @@
+from flask import abort, Flask, request, send_file 
+import os
+app = Flask (__name__)
+
+@app.route('/download')
+def download():
+    file = request.args.get('file', 'default.png')
+    
+    if '..' in file:
+        abort (400, 'Directory Traversal Detected')
+    
+    absolute_path = os.path.join(app.root_path, 'downloads', file)
+    print(absolute_path)
+    if os.path.isfile(absolute_path):
+        return send_file (absolute_path)
+    else:
+        abort (404, 'Requested File Not Found')
